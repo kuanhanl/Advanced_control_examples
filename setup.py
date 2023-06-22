b9e01d45392f83a914c690fc98463ba8401730fc
kuanhanl_DH4200 <kuanhanl@andrew.cmu.edu>
2023-06-22 15:12:56 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="advanced_control_examples",
+    version="1.0",
+    author="Kuan-Han Lin",
+    author_email="",
+    description="NMPC and MHE examples",
+    url="",
+    packages=find_packages()
+)
